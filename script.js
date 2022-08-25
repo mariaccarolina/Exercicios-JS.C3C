8f9a0770662099eafbbdeb422fdd11dfa2149245
Maria Carolina <demycar24@gmail.com>
2022-08-24 23:49:12 -0300

    exercicios

---
+++ script.js
@@ -0,0 +1,25 @@
+//1 - Escreva uma função JS para receber o salário mensal atual de um funcionário. Calcular e retornar o valor do novo salário com 22% de reajuste.
+
+//2 - O custo de um carro novo ao consumidor é a soma do custo de fábrica com a porcentagem do distribuidor e dos impostos (aplicados ao custo de fábrica). Supondo que o percentual do
+//distribuidor seja de 15% e os impostos de 30%, escrever uma função JS para que receba o custo de fábrica de um carro, calcular e retornar o custo final ao consumidor.
+
+//3 - Faça uma função JS que recebe quatro notas de um aluno, calcule e escreva no console a média final deste aluno. Considerar que a média é ponderada e que o peso das notas é 2, 3 e 4 e 7. A fórmula para o
+//cálculo da média final é: (n1*2 + n2*3 + n3*4 + n4*7)/16
+
+//4 - A jornada de trabalho semanal de um funcionário é de 40 horas. O funcionário que trabalhar mais de 40 horas receberá hora extra, cujo cálculo é o valor da hora regular com um acréscimo de
+//50%. Escreva uma função JS que recebe o número de horas trabalhadas em um mês, o salário por hora e escreva o salário total do funcionário, que deverá ser acrescido das horas extras, caso tenham sido
+//trabalhadas (considere que o mês possua 4 semanas exatas).
+
+//5 - Faça uma função JS para receber um número que é um código de usuário. Caso este código seja diferente de um código armazenado internamente no código JS (igual a 1234) deve ser apresentada
+//a mensagem ‘Usuário inválido!’. Caso o Código seja correto, deve ser lido outro valor que é a senha. Se esta senha estiver incorreta (a certa é 9999) deve ser mostrada a mensagem ‘senha
+//incorreta’. Caso a senha esteja correta, deve ser mostrada a mensagem ‘Acesso permitido’.
+
+//6 - Escreva uma função JS para receber as notas da 1a. e 2a. avaliações de um aluno, calcule e imprima a média (simples) desse aluno. Na execução, só devem ser aceitos valores válidos durante (0 a 10) para
+//cada nota.
+
+//7 - Receber um valor inteiro (aceitar somente valores entre 1 e 10) e escrever a tabuada de 1 a 10 do valor lido.
+
+//8 - Escreva uma função JS que imprima as seguintes seqüências de números: (1, 1 2 3 4 5 6 7 8 9 10) (2, 1 2 3 4 5 6 7 8 9 10) (3, 1 2 3 4 5 6 7 8 9 10) (4, 1 2 3 4 5 6 7 8 9 10) e assim
+//sucessivamente, até que o primeiro número (antes da vírgula), também chegue a 10.
+
+//Obs.: Procurem exercitar o uso de Arrays, funções com e sem retorno e também o uso de inputs html para receber os dados do usuário.
